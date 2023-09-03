4386c20b4bc15d76c709618dce2f39bf5239a546
Mohammed Thaier <70210166+itsMohammedThaier@users.noreply.github.com>
2023-09-03 15:10:14 +0300

    Create ex7_DiagonalLines.py

---
+++ env/Experiments/ex7_DiagonalLines.py
@@ -0,0 +1,41 @@
+"""
+    Experiment 7
+    Convert image to a set of diagonal lines, based on experiment 5
+    Showcase: https://www.instagram.com/p/CwuqOF0o62N/?img_index=1
+"""
+import sys, os
+from PIL import Image,ImageDraw
+
+c1=(0,0,0)
+c2=(200,200,200)
+with Image.open(sys.argv[1]) as im:
+    im1=im.convert('L')
+    im2=Image.new("RGB",im.size,color=c1)
+    draw=ImageDraw.Draw(im2)
+    
+    def Helper(xy,t):
+        l=xy[2]-xy[0]
+        if l<3:
+            return
+        s=0
+        for x in range(xy[0],xy[2]):
+            for y in range(xy[1],xy[3]):
+                s+=im1.getpixel((x,y))
+        if s/(l**2)<t:
+            return
+        
+        c=im1.getpixel((xy[0],xy[1]))
+        draw.line(xy, fill=(c,c,c))
+
+        
+        ll=l//2
+        t+=20
+        Helper((xy[0],xy[1],xy[0]+ll,xy[1]+ll), t)
+        Helper((xy[0],xy[1]+ll,xy[0]+ll,xy[3]), t)
+        Helper((xy[0]+ll,xy[1],xy[2],xy[1]+ll), t)
+        Helper((xy[0]+ll,xy[1]+ll,xy[2],xy[3]), t)
+
+
+    Helper((0,0,im.size[0]-1,im.size[1]-1),0)
+    im2.show()
+    im2.save("x.jpg")
