587e341bf1bbdd01ae9e96823f69ac89eae5b0ba
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-09-03 15:19:25 -1000

    ex7 support non-square images
    support for non-square images in ex7

--- env/Experiments/ex7_DiagonalLines.py
+++ env/Experiments/ex7_DiagonalLines.py
@@ -12,28 +12,32 @@ with Image.open(sys.argv[1]) as im:
     im1=im.convert('L')
     im2=Image.new("RGB",im.size,color=c1)
     draw=ImageDraw.Draw(im2)
-    
-    def Helper(xy,t):
-        l=xy[2]-xy[0]
-        if l<3:
+
+
+    def Helper(xy, t):
+        w = xy[2] - xy[0]
+        h = xy[3] - xy[1]
+        if w < 3 or h < 3:
             return
+
         s=0
         for x in range(xy[0],xy[2]):
             for y in range(xy[1],xy[3]):
                 s+=im1.getpixel((x,y))
-        if s/(l**2)<t:
-            return
-        
-        c=im1.getpixel((xy[0],xy[1]))
-        draw.line(xy, fill=(c,c,c))
 
-        
-        ll=l//2
-        t+=20
-        Helper((xy[0],xy[1],xy[0]+ll,xy[1]+ll), t)
-        Helper((xy[0],xy[1]+ll,xy[0]+ll,xy[3]), t)
-        Helper((xy[0]+ll,xy[1],xy[2],xy[1]+ll), t)
-        Helper((xy[0]+ll,xy[1]+ll,xy[2],xy[3]), t)
+        if s / (w * h) < t:
+            return
+
+        c = im1.getpixel((xy[0], xy[1]))
+        draw.line(xy, fill=(c, c, c))
+
+        hw = w // 2
+        hh = h // 2
+        t += 20
+        Helper((xy[0], xy[1], xy[0] + hw, xy[1] + hh), t)
+        Helper((xy[0], xy[1] + hh, xy[0] + hw, xy[3]), t)
+        Helper((xy[0] + hw, xy[1], xy[2], xy[1] + hh), t)
+        Helper((xy[0] + hw, xy[1] + hh, xy[2], xy[3]), t)
 
 
     Helper((0,0,im.size[0]-1,im.size[1]-1),0)
